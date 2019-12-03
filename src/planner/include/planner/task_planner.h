5d95ce8300137c56943557e8750a83c5bcebe630
Yang Zhang <youngzh@umich.edu>
2019-12-03 12:44:34 -0800

    add task planner

---
+++ src/planner/include/planner/task_planner.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <ros/ros.h>
+
+namespace tamp {
+namespace planner {
+class TaskPlanner {
+ public:
+  TaskPlanner();
+
+ private:
+};
+}  // namespace planner
+}  // namespace tamp
---
+++ src/planner/src/task_planner.cpp
@@ -0,0 +1,9 @@
+#include "planner/task_planner.h"
+
+namespace tamp {
+namespace planner {
+
+TaskPlanner::TaskPlanner() {}
+
+}  // namespace planner
+}  // namespace tamp
